fd8bb37425d70df5a67a60ffe9c5c2ae9c0fa51f
MinhVu <21522808@gm.uit.edu.vn>
2024-04-22 13:48:32 +0700

    Add font render manager

---
+++ core/src/com/mygdx/game/managers/Font.java
@@ -0,0 +1,98 @@
+package com.mygdx.game.managers;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+
+public enum Font {
+    MANAGER;
+
+    final FreeTypeFontGenerator.FreeTypeFontParameter fontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+    // set font
+    private final FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(
+            Gdx.files.internal("fonts/Hyperspace Bold.ttf"));
+    private final GlyphLayout layout = new GlyphLayout();
+
+    /**
+     * Formats a font.
+     *
+     * @param size - size of the font.
+     * @return formatted bitmap font.
+     */
+    public BitmapFont set(int size) {
+
+        return set(size, Color.WHITE);
+    }
+
+    /**
+     * Formats a font.
+     *
+     * @param size  - size of the font.
+     * @param color - color of the font.
+     * @return formatted bitmap font.
+     */
+    public BitmapFont set(int size, Color color) {
+        // set parameters for the font
+        fontParameter.size = size;
+        fontParameter.color = color;
+        return fontGenerator.generateFont(fontParameter);
+    }
+
+    /**
+     * Displays left-justify a message in assigned coordinates.
+     *
+     * @param sb      SpriteBatch
+     * @param font    Font configuration.
+     * @param message Message to display
+     * @param width   Assigned horizontal
+     * @param height  Assigned vertical
+     */
+    public void left(SpriteBatch sb, BitmapFont font, String message, float width, float height) {
+        sb.begin();
+        layout.reset();
+        layout.setText(font, message);
+        font.draw(sb, layout, width, height);
+        sb.end();
+        layout.reset();
+    }
+
+    /**
+     * Displays right-justify a message in assigned coordinates.
+     *
+     * @param sb      SpriteBatch
+     * @param font    Font configuration.
+     * @param message Message to display
+     * @param width   Assigned horizontal
+     * @param height  Assigned vertical
+     */
+    public void right(SpriteBatch sb, BitmapFont font, String message, float width, float height) {
+        sb.begin();
+        layout.reset();
+        layout.setText(font, message);
+        font.draw(sb, layout, width - layout.width, height);
+        sb.end();
+        layout.reset();
+    }
+
+    /**
+     * Displays centered a message in assigned coordinates.
+     *
+     * @param sb      SpriteBatch
+     * @param font    Font configuration.
+     * @param message Message to display
+     * @param width   Assigned horizontal
+     * @param height  Assigned vertical
+     */
+    public void centered(SpriteBatch sb, BitmapFont font, String message, float width, float height) {
+        sb.begin();
+        layout.reset();
+        layout.setText(font, message);
+        font.draw(sb, layout, width - layout.width / 2, height);
+        sb.end();
+        layout.reset();
+    }
+
+}

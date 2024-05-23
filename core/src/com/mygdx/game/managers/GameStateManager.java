0c3fc58ca25049767d88e7eb692ca5ce9c26d05c
HieuLe <lthieu9601@gmail.com>
2024-05-23 08:42:44 +0700

    Update Game state manager

--- core/src/com/mygdx/game/managers/GameStateManager.java
+++ core/src/com/mygdx/game/managers/GameStateManager.java
@@ -4,11 +4,11 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.mygdx.game.Game;
-import com.mygdx.game.gamestates.GameState;
-import com.mygdx.game.gamestates.MenuState;
+import com.mygdx.game.gamestates.*;
 
 public class GameStateManager {
     private GameState gameState;
+
     public enum State {
         MENU, PLAY, HIGHS_CORES, OPTIONS, GAME_OVER
     }
@@ -24,6 +24,8 @@ public class GameStateManager {
     private static OptionKeys optionKeys;
 
     private static PlayMode playMode;
+
+    private static boolean newGame;
 
     public final SpriteBatch batch = new SpriteBatch();
     public final ShapeRenderer renderer = new ShapeRenderer();
@@ -52,10 +54,16 @@ public class GameStateManager {
             case PLAY:
                 break;
             case HIGHS_CORES:
+                Game.setCameraPosition();
+                gameState = new HighScoreState(this);
                 break;
             case OPTIONS:
+                Game.setCameraPosition();
+                gameState = new OptionsState(this);
                 break;
             case GAME_OVER:
+                Game.setCameraPosition();
+                gameState = new GameOverState(this);
                 break;
         }
     }
@@ -66,5 +74,29 @@ public class GameStateManager {
 
     public void draw() {
         gameState.draw();
+    }
+
+    public static PlayMode getPlayMode() {
+        return playMode;
+    }
+
+    public static void setPlayMode(PlayMode playMode) {
+        GameStateManager.playMode = playMode;
+    }
+
+    public static OptionKeys getOptionsKeys() {
+        return GameStateManager.optionKeys;
+    }
+
+    public void setOptionsKeys(OptionKeys optionKeys) {
+        GameStateManager.optionKeys = optionKeys;
+    }
+
+    public static void startNewGame() {
+        newGame = !newGame;
+    }
+
+    public static boolean isNewGame() {
+        return newGame;
     }
 }
